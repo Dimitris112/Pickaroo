39d359a4323aed375354430533068ae071b8e3f6
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-12-27 21:00:04 +0000

    Add App.css custom styling

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = '/api';
+axios.defaults.baseURL = "/api";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data"; // images and more
 axios.defaults.withCredentials = true; // required for cookies
 
