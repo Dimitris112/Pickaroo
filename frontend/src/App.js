1aa80553cb2e4cb79af2a84849e79948f96371a0
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-12-28 11:39:46 +0000

    Update App.js with routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,45 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Suspense } from "react";
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
+
+const HomePage = React.lazy(() => import("./pages/homepage/HomePage"));
+const SignUpForm = React.lazy(() => import("./pages/auth/SignupPage"));
+const SignInForm = React.lazy(() => import("./pages/auth/LoginPage"));
+const ReviewsPage = React.lazy(() => import("./pages/reviews/ReviewsPage"));
+const ReviewCreateForm = React.lazy(() =>
+  import("./pages/reviews/ReviewCreateForm")
+);
+const ReportsPage = React.lazy(() => import("./pages/reports/ReportsPage"));
+const NotFound = React.lazy(() => import("./components/NotFound"));
+
+const Loader = () => <div className={styles.Loader}>Loading...</div>;
 
 function App() {
-  return <div>It's on it's way.</div>;
+  return (
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Suspense fallback={<Loader />}>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/signin" component={SignInForm} />
+            <Route exact path="/signup" component={SignUpForm} />
+            <Route exact path="/reviews" component={ReviewsPage} />
+            <Route
+              exact
+              path="/reviews/create"
+              component={ReviewCreateForm}
+            />
+            <Route exact path="/reports" component={ReportsPage} />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
